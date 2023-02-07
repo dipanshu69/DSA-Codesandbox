96b6aa1febd39bdad52670492af3d16a7e8e56f2
Dipanshu <deepanshu.patil69@gmail.com>
2023-02-07 13:45:16 +0000

    /Factorial of a number

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-const fibSeries = (n) => {
-  let fib = [0, 1];
-  if (n === 2) {
-    return fib;
-  } else {
-    for (let i = 2; i < n; i++) {
-      fib[i] = fib[i - 1] + fib[i - 2];
-    }
-    return fib;
-  }
-};
+//fibSeries
 
-console.log(fibSeries(7));
-//Big-O = O(n);
+// const fibSeries = (n) => {
+//   let fib = [0, 1];
+//   if (n === 2) {
+//     return fib;
+//   } else {
+//     for (let i = 2; i < n; i++) {
+//       fib[i] = fib[i - 1] + fib[i - 2];
+//     }
+//     return fib;
+//   }
+// };
+
+// console.log(fibSeries(7));
+// //Big-O = O(n);
+
+//Factorial of a number
+
+// const factorial = (n) => {
+//   let result = 1;
+//   for (let i = 2; i <= n; i++) {
+//     result = result * i;
+//   }
+//   return result;
+// };
+
+// console.log(factorial(5));
+
+// //Big-O = O[n];
